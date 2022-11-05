6f9e723d62dbed99294a86981c12923b69b5a095
geek.op <Rnoroozi2010@gmail.com>
2022-11-04 21:00:11 -0400

    HALLOOOOO DAYYYYYY 2222

--- day2/main.c
+++ day2/main.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 
 int main(){
+#11111111111111111111111111
 const int a=10;
 const int b=50;
-int x;
-x=a+b;
+#22222222222222222222222222222222
+int x=10;
+x ++;
 printf("%d",x);
-
-
+-- x;
+printf("%d",x);
+#3333333333333333333333333333333
+int m,n;
+m=20;
+n=30;
+int y;
+y=m+n;
+printf("\n\n\n\n%d",y);
+y=n-m;
+printf("\n\n%d",y);
+y=m*n;
+printf("\n\n%d",y);
+m=50;
+n=20;
+y=m/n;
+printf("\n\n\n\n%d",y);
 
 }
