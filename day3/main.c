5195b003c263a42a8ce101a522ac4f70db5070a7
GEEKGEEKOP <geekgeek.op@gmail.com>
2022-11-09 21:07:43 -0500

    commit amalgar

--- day3/main.c
+++ day3/main.c
@@ -7,12 +7,24 @@ x=10;
 int c;
 c=x*M;
 printf("\n%d\n",c);
-int z;
-scanf("\n%d\n",z);
-printf("\n%d\n",z);
+
 if (M == 90 && x == 10){
 printf("\nHALLO Im PROGRAMMER\n");
 }
+printf("\n---------------------------------------------\n\b");
+int y,z;
+x=1;
+y=2;
+z=3;
+int aa;
+aa=++x;
+printf("\n%d\n",aa);//2
+aa=--y;
+printf("\n%d\n",aa);//1
+aa=y+x;
+printf("\n%d\n",aa);//3
+
+
 
 return 0;
 }
