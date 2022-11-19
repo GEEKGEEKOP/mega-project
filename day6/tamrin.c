0dbac0bd923750228429c8c000ee4bb548236df0
GEEKGEEKOP <geekgeek.op@gmail.com>
2022-11-18 19:03:37 -0500

    HHHHHHHHHHHH

---
+++ day6/tamrin.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define PI 3.14
+int main(void){
+int r;
+float area,p;
+printf("\nENTER R ? ",r);
+scanf("%d",&r);
+area=r *r*PI;//masahat
+p=(r+r) * PI;
+printf("area = %f ,, p = %f\n",area,p);
+
+
+
+
+
+}
