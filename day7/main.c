00fe5089f5e2174cb39e004a8cb1767e4981415f
GEEKGEEKOP <geekgeek.op@gmail.com>
2022-11-23 21:16:45 -0500

    mega COMMIT

---
+++ day7/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h> 
+int main(){
+
+
+
+
+printf("\nHALLO\n");
+int a,b,i;
+a=1;
+b=1;
+for (i = 0;i == 50;i++){
+a=a+i;
+printf("%d\n",i);
+}
+printf("%d\n",a);
+
+gotoxy(10,20);
+
+getch();
+}
