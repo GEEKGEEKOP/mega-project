ab4d353d125f4bc3ff1780416e285df423dd6d75
GEEKGEEKOP <geekgeek.op@gmail.com>
2022-12-07 21:38:23 -0500

    UMMM A MOMENT

--- day8/main.c
+++ day8/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 main(){
-	printf("HELLO WORLD");
+	printf("\nHELLO WORLD\n");
+	int a,b,i,c;
+	a=0;
+	b=1;
+	c=0;
+	for (i=1;i <= 300;i++){
+		c=a + (b++);
+		printf("%d\n",c);
+
+	}
 
 }
